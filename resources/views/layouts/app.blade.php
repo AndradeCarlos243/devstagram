6a2c53c42f220685d1cc89a72624b179dc56c426
AndradeCarlos243 <thehamster_gamer@outlook.com>
2023-12-12 22:43:05 -0600

    Añedido tailwindcss y navegación principal

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Devstagram - @yield('titulo')</title>
+        @vite('resources/css/app.css')
+    </head>
+    <body class="bg-gray-100">
+        <header class="p-5 border-b bg-white shadow">
+            <div class="container mx-auto flex justify-between
+            items-center">
+                <h1 class="text-3xl font-black">
+                    Devstagram
+                </h1>
+
+                <nav class="flex gap-2 items-center">
+                    <a class="font-bold uppercase text-gray-600 
+                    text-sm" href="#">Login</a>
+                    <a class="font-bold uppercase text-gray-600 
+                    text-sm" href="#">Crear cuenta</a>
+                </nav>
+            </div>
+        </header>
+
+        <main class="container mx-auto mt-10">
+            <h2 class="font-black text-center text-3xl mb-10">
+                @yield('titulo')
+            </h2>
+            @yield('contenido')
+        </main>
+
+        <footer class="text-center p-5 text-gray-500 font-bold
+        uppercase">
+            DevStagram - Todos los derechos reservados. {{now()->year}}
+        </footer>
+    </body>
+</html>
---
+++ resources/views/principal.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('titulo')
+    Principal
+@endsection
+
+
+@section('contenido')
+    <p>Estamos en el contenido principal</p>
+@endsection
